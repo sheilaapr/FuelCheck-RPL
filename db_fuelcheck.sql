130f23f8d692dddace10a358ebb80482e5b91022
Sheila Apriliani Putri <230605110005@student.uin-malang.ac.id>
2025-06-20 14:55:06 +0700

    Add files via upload

--- db_fuelcheck.sql
+++ db_fuelcheck.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 20, 2025 at 02:22 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: localhost:3306
+-- Generation Time: Jun 20, 2025 at 07:54 AM
+-- Server version: 9.0.1
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,12 +28,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin_verifikasi` (
-  `id` int(11) NOT NULL,
-  `admin_id` int(11) NOT NULL,
-  `laporan_id` int(11) NOT NULL,
-  `status_verifikasi` enum('valid','tidak valid') NOT NULL,
-  `catatan` text DEFAULT NULL,
-  `verified_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `id` int NOT NULL,
+  `admin_id` int NOT NULL,
+  `laporan_id` int NOT NULL,
+  `status_verifikasi` enum('valid','tidak valid') COLLATE utf8mb4_general_ci NOT NULL,
+  `catatan` text COLLATE utf8mb4_general_ci,
+  `verified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -43,13 +43,13 @@ CREATE TABLE `admin_verifikasi` (
 --
 
 CREATE TABLE `banding` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `spbu_id` int(11) NOT NULL,
-  `laporan_id` int(11) NOT NULL,
-  `verifikasi_id` int(11) NOT NULL,
-  `isi_banding` text NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `spbu_id` int NOT NULL,
+  `laporan_id` int NOT NULL,
+  `verifikasi_id` int NOT NULL,
+  `isi_banding` text COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -59,11 +59,11 @@ CREATE TABLE `banding` (
 --
 
 CREATE TABLE `bukti_laporan` (
-  `id` int(11) NOT NULL,
-  `laporan_id` int(11) NOT NULL,
-  `file_path` varchar(255) NOT NULL,
-  `file_type` enum('image','video') NOT NULL,
-  `uploaded_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `id` int NOT NULL,
+  `laporan_id` int NOT NULL,
+  `file_path` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `file_type` enum('image','video') COLLATE utf8mb4_general_ci NOT NULL,
+  `uploaded_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -73,11 +73,11 @@ CREATE TABLE `bukti_laporan` (
 --
 
 CREATE TABLE `feedback` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `admin_id` int(11) DEFAULT NULL,
-  `nama` varchar(255) DEFAULT NULL,
-  `pesan` text DEFAULT NULL
+  `id` int NOT NULL,
+  `user_id` int DEFAULT NULL,
+  `admin_id` int DEFAULT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `pesan` text COLLATE utf8mb4_general_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -87,13 +87,13 @@ CREATE TABLE `feedback` (
 --
 
 CREATE TABLE `laporan` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `spbu_id` int(11) NOT NULL,
-  `transaksi_id` int(11) NOT NULL,
-  `deskripsi` text NOT NULL,
-  `status` enum('pending','verifikasi','selesai') NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `spbu_id` int NOT NULL,
+  `transaksi_id` int NOT NULL,
+  `deskripsi` text COLLATE utf8mb4_general_ci NOT NULL,
+  `status` enum('pending','verifikasi','selesai') COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -103,12 +103,12 @@ CREATE TABLE `laporan` (
 --
 
 CREATE TABLE `rating` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `spbu_id` int(11) NOT NULL,
-  `nilai` int(11) NOT NULL,
-  `komentar` text DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `spbu_id` int NOT NULL,
+  `nilai` int NOT NULL,
+  `komentar` text COLLATE utf8mb4_general_ci,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -118,10 +118,25 @@ CREATE TABLE `rating` (
 --
 
 CREATE TABLE `spbu` (
-  `id` int(11) NOT NULL,
-  `nama_spbu` varchar(255) NOT NULL,
-  `lokasi` varchar(255) NOT NULL
+  `id` int NOT NULL,
+  `nama_spbu` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `lokasi` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ulasan`
+--
+
+CREATE TABLE `ulasan` (
+  `id` int NOT NULL,
+  `id_user` int DEFAULT NULL,
+  `spbu` varchar(255) DEFAULT NULL,
+  `rating` int DEFAULT NULL,
+  `komentar` text,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -130,12 +145,24 @@ CREATE TABLE `spbu` (
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `role` enum('user','admin') NOT NULL
+  `id` int NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `role` enum('user','admin') COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `nama`, `email`, `password`, `role`) VALUES
+(1, 'sheila', 'shei@gmail.com', '$2y$10$GwCcUFf81i8gOlKl5zf/j.AwhCNgEXT./WNWgd1Hyl5d2ck0xTNnW', 'user'),
+(2, 'IBRANN', 'LALAA@gmail.com', '$2y$10$Aj/1ZVZ9xXYr4ax3PI8fneeex/jL/ByvJhXcYQbm6gqqePpaeticG', 'user'),
+(3, 'sheila', 'shei2@gmail.com', '$2y$10$h1iqMRHTbeZljlyG87NYEuMTeITHS8Qo7K5ni0RIyNSIxzPxSzYOK', 'user'),
+(4, 'nabila', 'nandaabintang@gmail.com', '$2y$10$vg4tf.mgf3I6Ixms6zM8COwRXOeiLDIT2XodFOZSbM6D9aeJKrAUy', 'user'),
+(6, 'pakAgung', 'nndaaa@gmail.com', '$2y$10$PFYXbpluu/yN7zLQSc69kOxKKc.EVhOHhJwpzagKbjiutBkLIAGfe', 'admin'),
+(7, 'Sheila Apriliani Putri', '230605110005@student.uin-malang.ac.id', '$2y$10$3X6iUGToyBd9XYev/IW4Pe75pNXh919WhjQJv0QZcwLBJj31oruhq', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -197,6 +224,12 @@ ALTER TABLE `spbu`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `ulasan`
+--
+ALTER TABLE `ulasan`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -211,49 +244,55 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admin_verifikasi`
 --
 ALTER TABLE `admin_verifikasi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `banding`
 --
 ALTER TABLE `banding`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `bukti_laporan`
 --
 ALTER TABLE `bukti_laporan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `laporan`
 --
 ALTER TABLE `laporan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `rating`
 --
 ALTER TABLE `rating`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `spbu`
 --
 ALTER TABLE `spbu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ulasan`
+--
+ALTER TABLE `ulasan`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
